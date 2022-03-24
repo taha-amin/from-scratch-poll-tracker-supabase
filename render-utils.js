a2a38b466fd82093a1ad3838387233c19c60737b
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-24 12:41:23 -0700

    created renderPoll function.

--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,12 @@
 export function renderPoll(poll) {
-    
-}
+    //render and append
+    const newPollEl = document.createElement('div');
+    const newQuestionEl = document.createElement('p');
+    const newOptionOneEl = document.createElement('p');
+    const newOptionTwoEl = document.createElement('p');
+    const newVotesOneEl = document.createElement('p');
+    const newVotesTwoEl = document.createElement('p');
 
-export function 
+    newPollEl.classList.add('pol');
+    newQuestionEl.textContent = poll.question;
+}
