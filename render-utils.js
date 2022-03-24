8d2098463757c72954baaa6cc96ab142949f912c
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-24 12:45:00 -0700

    finished writing renderPoll func.

--- render-utils.js
+++ render-utils.js
@@ -9,4 +9,18 @@ export function renderPoll(poll) {
 
     newPollEl.classList.add('pol');
     newQuestionEl.textContent = poll.question;
+    newOptionOneEl.textContent = poll.option_1;
+    newOptionTwoEl.textContent = poll.option_2;
+    newVotesOneEl.textContent = poll.votes_1;
+    newVotesTwoEl.textContent = poll.votes_2;
+
+    newPollEl.append(
+        newQuestionEl,
+        newOptionOneEl,
+        newVotesOneEl,
+        newOptionTwoEl,
+        newVotesTwoEl,
+    );
+
+    return newPollEl;
 }
