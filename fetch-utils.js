0aa7cd4e7e874dec638df246ce339786269e8ef4
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-24 11:15:13 -0700

    created async functions signupUser and async function signInUser.

--- fetch-utils.js
+++ fetch-utils.js
@@ -10,3 +10,15 @@ export async function createGame(game) {
     
     return response.body;
 }
+
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
