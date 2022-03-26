0ad249ea95d79f8ac002be6bc5ba9562ff59ad45
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-25 17:34:20 -0700

    removed unnecessary code

--- render-utils.js
+++ render-utils.js
@@ -1,15 +1,3 @@
-// export function renderPoll(poll) {
-//     const div = document.createElement('div');
-//     const option1Div = renderPoll(poll.option_1, poll.votes_1);
-//     const option2Div = renderPoll(poll.option_2, poll.votes_2);
-
-//     div.append(option1Div, option2Div);
-
-//     div.classList.add('poll');
-
-//     return div;
-// }
-
 export function renderPoll(poll) {
     // render and append
     const newPollEl = document.createElement('div');
